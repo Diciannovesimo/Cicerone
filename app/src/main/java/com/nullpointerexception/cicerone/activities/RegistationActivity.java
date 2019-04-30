1da69d9a93627ee33f3ac9464f30371c9f5157ab
Cioscos <claudiocicca1998@gmail.com>
2019-04-30 09:56:19 +0200

    First commit for Registration activity

---
+++ app/src/main/java/com/nullpointerexception/cicerone/activities/RegistationActivity.java
@@ -0,0 +1,16 @@
+package com.nullpointerexception.cicerone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nullpointerexception.cicerone.R;
+
+public class RegistationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
