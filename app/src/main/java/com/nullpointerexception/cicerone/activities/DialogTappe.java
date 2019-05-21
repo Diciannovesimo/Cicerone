8edc15a187c15cff92d82cc3b7c4f88611c390d5
Cioscos <claudiocicca1998@gmail.com>
2019-05-21 22:31:00 +0200

    Commit 3 -Miglioramento sostanziale grafica Activity -Aggiunta Autocomplete text per inserimento luoghi.

--- app/src/main/java/com/nullpointerexception/cicerone/activities/DialogTappe.java
+++ app/src/main/java/com/nullpointerexception/cicerone/activities/DialogTappe.java
@@ -3,14 +3,21 @@ package com.nullpointerexception.cicerone.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.nullpointerexception.cicerone.R;
 
 public class DialogTappe extends AppCompatActivity {
 
+    private Button mCreateStage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_tappe);
+
+        mCreateStage = findViewById(R.id.createStage_btn);
+
+
     }
 }
--- app/src/main/java/com/nullpointerexception/cicerone/activities/ItineraryActivity.java
+++ app/src/main/java/com/nullpointerexception/cicerone/activities/ItineraryActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.kinda.mtextfield.ExtendedEditText;
-import com.kinda.mtextfield.TextFieldBoxes;
 import com.nullpointerexception.cicerone.R;
 
 import androidx.appcompat.app.AlertDialog;
@@ -17,20 +21,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 
+//Google SDk
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TimePicker;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+
+import studio.carbonylgroup.textfieldboxes.TextFieldBoxes;
 
 public class ItineraryActivity extends AppCompatActivity {
 
-    private EditText mLuogo, mPuntoIncontro, mData, mOra, mMaxPart, mLingua, mCompenso;
+    private EditText mLuogo, mPuntoIncontro, mData, mOra, mMaxPart, mLingua, mCompenso, mMaps;
     private ExtendedEditText mDescrizione;
-    private FloatingActionButton newPoint;
+    private ImageView mAddStage;
+    private TextFieldBoxes luogo_box;
+    private int AUTOCOMPLETE_REQUEST_CODE = 1;
 
     //Datepicker object
     Calendar calendar;
@@ -45,6 +61,14 @@ public class ItineraryActivity extends AppCompatActivity {
         toolbar.setTitle("Nuovo itinerario");
         setSupportActionBar(toolbar);
 
+        // Initialize Places.
+        Places.initialize(getApplicationContext(), getResources().getString(R.string.place_KEY));
+
+        // Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
         mLuogo = findViewById(R.id.place_et);
         mPuntoIncontro = findViewById(R.id.meetPlace_et);
         mData = findViewById(R.id.date_picker);
@@ -53,10 +77,16 @@ public class ItineraryActivity extends AppCompatActivity {
         mLingua = findViewById(R.id.language_et);
         mCompenso = findViewById(R.id.recompense_et);
         mDescrizione = findViewById(R.id.description_et);
-        newPoint = findViewById(R.id.fab);
+        mAddStage = findViewById(R.id.addStage_btn);
+        luogo_box = findViewById(R.id.luogo_box);
 
         Window window = getWindow();
         window.setStatusBarColor(Color.parseColor("#FF5500"));
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         MaterialSpinner spinner = findViewById(R.id.spinner_valute);
         spinner.setItems("€ Euro", "$ Dollaro", "£ Sterlina");
@@ -114,7 +144,7 @@ public class ItineraryActivity extends AppCompatActivity {
             }
         });
 
-        newPoint.setOnClickListener(new View.OnClickListener() {
+        mAddStage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final AlertDialog dialog;
@@ -123,5 +153,34 @@ public class ItineraryActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+
+        luogo_box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Autocomplete.IntentBuilder(
+                        AutocompleteActivityMode.FULLSCREEN, fields)
+                        .build(v.getContext());
+                startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i("it_view", "Place: " + place.getName() + ", " + place.getId());
+                if(!mLuogo.getText().toString().isEmpty())
+                    mLuogo.setText("");
+                else
+                    mLuogo.setText(place.getName());
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i("it_view", status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 }
