3dbe196bc6efe27f25830611af42dc4411384b86
Luca <luca.garofalo1369@gmail.com>
2019-06-19 19:53:13 +0200

    Creata home.

--- app/src/main/java/com/nullpointerexception/cicerone/fragments/HomeFragment.java
+++ app/src/main/java/com/nullpointerexception/cicerone/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.nullpointerexception.cicerone.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.nullpointerexception.cicerone.R;
+import com.nullpointerexception.cicerone.activities.FindItineraryActivty;
+import com.nullpointerexception.cicerone.activities.ItineraryCreationActivity;
 import com.nullpointerexception.cicerone.activities.MainActivity;
 import com.nullpointerexception.cicerone.components.AuthenticationManager;
 
@@ -17,11 +20,12 @@ public class HomeFragment extends Fragment
 
     public HomeFragment() { }
 
+    public View buttonCreateItinerary, buttonFindItinerary;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -30,12 +34,21 @@ public class HomeFragment extends Fragment
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        buttonCreateItinerary = view.findViewById(R.id.buttonCreateItinerary);
+        buttonFindItinerary = view.findViewById(R.id.buttonFindItinerary);
+
         if(getActivity() != null && ((MainActivity) getActivity()).getSupportActionBar() != null)
             ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.menu_home);
 
         if(AuthenticationManager.get().getUserLogged() != null)
             ((TextView) view.findViewById(R.id.welcomeText)).setText(
                     "Ciao, " + AuthenticationManager.get().getUserLogged().getName() + " !");
+
+        buttonCreateItinerary.setOnClickListener(v ->
+                startActivity(new Intent(getContext(), ItineraryCreationActivity.class)));
+
+        buttonFindItinerary.setOnClickListener(v ->
+                startActivity(new Intent(getContext(), FindItineraryActivty.class)));
 
         return view;
     }
